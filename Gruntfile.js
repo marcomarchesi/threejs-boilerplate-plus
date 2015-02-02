0b0004c5e662f023b61d24318aec9076cb6c540c
Marco Marchesi <marco.marchesi@unibo.it>
2015-02-02 14:53:22 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['start.js',
+              'js/**/*.js',
+              ],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    // qunit: {
+    //   files: ['test/**/*.js']
+    // },
+    jshint: {
+      files: ['Gruntfile.js','test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    nodemon: {
+      dev: {
+        script: 'start.js'
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-nodemon');
+  //grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  //grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify','nodemon','watch']);
+
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# threejs-boilerplate-plus
+A template for threejs-based projects
+
+# license
+Under MIT license
