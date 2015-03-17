2e1145f10c182cdb4e3d6486b22234024b09e114
marcomarchesi <marchesimarco@gmail.com>
2015-03-17 20:21:58 +0100

    added websocket for Glove

--- dist/threejs-boilerplate-plus.js
+++ dist/threejs-boilerplate-plus.js
@@ -1,19 +1,26 @@
 /* threejs-boilerplate-plus
 */
 
-// var libovr = require("node-ovrsdk");
 
 var express = require('express');
-var chalk = require('chalk');
-var fs = require('fs');
+var io = require('socket.io');
 var app = express();
-var port = 8080;
+var port = 8888;
 
 app.use(express.static(__dirname + '/public')).listen(port);
 
 // app.listen(port);
 console.log("listening port " + port);
 
+var socket = io.listen(app.listen(port));
+
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
+});
+
+socket.on('connection',function(socket){
+  socket.on('message',function(data){
+    console.log('message received from server is: ' + data);
+    io.sockets.emit('message',data);
+  });
 });
--- start.js
+++ start.js
@@ -3,14 +3,25 @@
 
 
 var express = require('express');
+var io = require('socket.io');
+var http = require('http');
 var app = express();
 var port = 8888;
 
-app.use(express.static(__dirname + '/public')).listen(port);
+app.use(express.static(__dirname + '/public'));
 
 // app.listen(port);
 console.log("listening port " + port);
 
+var io = require('socket.io').listen(app.listen(port));
+
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
+});
+
+io.sockets.on('connection', function (socket){
+  socket.on('message',function (data){
+    console.log('message received from server is: ' + data);
+    io.sockets.emit('message',data);
+  });
 });
