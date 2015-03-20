c4b8cc54ea2fff545e5f11b291414dc858ec8b98
marcomarchesi <marchesimarco@gmail.com>
2015-03-20 19:09:27 +0100

    fixed merge

--- start.js
+++ start.js
@@ -4,22 +4,14 @@
 
 var express = require('express');
 var app = express();
-var port = 8888;
+var port = 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')).listen(port);
 
 // app.listen(port);
 console.log("listening port " + port);
-
-var io = require('socket.io').listen(app.listen(port));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
-io.sockets.on('connection', function (socket){
-  socket.on('message',function (data){
-    console.log('message received from server is: ' + data);
-    io.sockets.emit('message',data);
-  });
-});
